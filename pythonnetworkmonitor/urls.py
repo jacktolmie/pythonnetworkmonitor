083c7c4398e4bfb2fe0c1e6fc3fd13f935ccecaf
Jack Tolmie <jacktolmie@hotmail.com>
2025-07-22 08:42:27 -0700

    Still trouble shooting db errors in docker

--- pythonnetworkmonitor/urls.py
+++ pythonnetworkmonitor/urls.py
@@ -13,6 +13,5 @@ urlpatterns = [
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
     path('api/token/verify/', TokenVerifyView.as_view(), name='token_verify'),
     path('monitor/', include('network_monitor.urls')),
-    path("", include("network_monitor.urls"))
 
 ]
